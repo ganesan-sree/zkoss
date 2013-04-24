b080ff7f440e3a034b5c26c6fa122e446aa53766
ganesan-sree <ganesan@easymedmobile.com>
2013-04-24 18:37:51 +0530

    calander

---
+++ zksample/src/com/calender/MyCalendarEvent.java
@@ -0,0 +1,57 @@
+package com.calender;
+
+import java.util.Date;
+
+import org.zkoss.calendar.api.CalendarEvent;
+
+public class MyCalendarEvent implements  CalendarEvent
+{
+	@Override
+	public Date getBeginDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getContent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getContentColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Date getEndDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHeaderColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getZclass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
